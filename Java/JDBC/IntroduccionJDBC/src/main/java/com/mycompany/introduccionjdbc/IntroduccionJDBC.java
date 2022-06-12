59cbde41fe866f4a482b1d1d16cad6da269937c9
Matias Basilotta <matiasbasilotta@gmail.com>
2022-06-12 00:12:50 -0300

    12-6

---
+++ Java/JDBC/IntroduccionJDBC/src/main/java/com/mycompany/introduccionjdbc/IntroduccionJDBC.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.introduccionjdbc;
+
+
+public class IntroduccionJDBC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
